78cd2bc199138e14be5be81ace366086dc0d6fb5
bBwhackeR <bBwhackeR69@github.com>
2020-09-22 00:54:37 +0530

    uploading the bot on github

---
+++ config.py
@@ -0,0 +1,19 @@
+# To avoid Reddit's rate limit. 3 is ideal.
+wait_time = 0
+
+# Your Discord webhook settings.
+webhook_url = "https://discordapp.com/api/webhooks/756973142408429688/R3MloD3Dz5PerYEo_NwyBgO5AU1bsraGZbXoezuH8WYYMK06y2QHtl8kzRJ4MNAEIMm2"
+embed_title = "New post!"
+
+# Max post limit, 10 is recommended
+limit = 42
+
+# Add subreddits to track. You can use as much as subreddit you want.
+subreddits = ['meme' , 'memes' , 'AmongUs' , 'funny' , 'terriblefacebookmemes' , 'PewdiepieSubmissions' , 'marvelmemes' , 'gaming' , 'rareinsults' , 'insaneparents']
+
+# Reddit information
+username = "bB_SeNpai"
+password = "Bhavya_gosai@05081206"
+client_id = "LjWIaS5ThCt2Qw"
+client_secret = "eHKMX5qd2ws7nRMVQq2ls6yQ1jg"
+user_agent = "REDDIT TO DISCORD--SEND NUDES ASAP!"
