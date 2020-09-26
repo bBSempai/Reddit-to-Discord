62dcfac146e6e498e499e10b822945927bb878e3
bBSempai <bBSempai@github.com>
2020-09-26 14:09:50 +0530

    thelast

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 from reddit_to_discord import index
 
 
 def test_index():
     assert index()=="YESS"      
-=======
-from reddit_to_discord.index import index
-
-
-def test_index():
-    assert index()=="YESS"      
->>>>>>> ea9e68c6407664930ed7f963640a7b4d0437dc4f
