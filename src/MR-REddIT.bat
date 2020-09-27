fb0ec479158ea138b344c52ed9da18ff0d6deee9
bBSempai <bBSempai@github.com>
2020-09-28 00:41:53 +0530

    del

---
+++ src/MR-REddIT.bat
@@ -0,0 +1,1 @@
+python reddit_to_discord.py
---
+++ src/config.py
@@ -0,0 +1,19 @@
+# To avoid Reddit's rate limit. 3 is ideal.
+wait_time = 0
+
+# Your Discord webhook settings.
+webhook_url = "https://discordapp.com/api/webhooks/758227146782277652/IIQfAWgwsB2y3Wsn92Q3NasUVqGgXXoNIYfLhuvEz57Lnqw03gByYdSTmIDQEzNjvagP"
+embed_title = "New post!"
+
+# Max post limit, 10 is recommended
+limit = 42
+
+# Add subreddits to track. You can use as much as subreddit you want.
+subreddits = ['DarkMemesForDarkHumou' , 'DarkMemesAndHumor' , 'MakeMeSuffer']
+
+# Reddit information
+username = "bB_SeNpai"
+password = "Bhavya_gosai@05081206"
+client_id = "H5rOQdu7mzEfbQ"
+client_secret = "PYndJnFO2ZWWKwRC8R7Q943NUdY"
+user_agent = "REDDIT TO DISCORD--SEND NUDES ASAP!"
